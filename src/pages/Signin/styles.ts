34f292a646afc04da77e75d9322e025a4d5922e8
ismael <ismael.vargas@fundacred.org.br>
2022-07-25 01:29:33 -0300

    Criando componentes Button e Input

---
+++ src/pages/Signin/styles.ts
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signInBackground from '../../assets/iserviceOK.jpg'
+
+export const Container = styled.div`
+  height: 100vh;
+  
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  width: 100%;
+  max-width: 900px;
+
+  img{
+    height: 135px;
+    display: flex;
+  }
+  
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+    
+    h1 {
+      margin-bottom: 24px;
+    }
+    
+    button {
+      background: #ff9000;
+      height: 56px;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      color: #312e38;
+      width: 100%;
+      font-weight: 500;
+      margin-top: 16px;
+      transition: background-color 0.2s;
+      
+      &:hover {
+        background: ${shade(0.2, '#ff9000')};
+      }
+    }
+    
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }  
+  
+  > a {
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    
+    display: flex;
+    align-items: center;
+    
+    svg {
+      margin-right: 16px;
+    }
+   
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+  }
+
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackground}) no-repeat center;
+  background-size: cover;
+`;
